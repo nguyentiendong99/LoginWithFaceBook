124f2be0d25a037f1e76eac0ca5825d8d00a098c
= <=>
2020-09-02 10:59:39 +0700

    Login with facebook in Springboot

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+@EnableOAuth2Sso
+public class DemoApplication {
+    @GetMapping("/")
+    public String Welcome(Principal principal){
+        Map<String, Object> details = (Map<String , Object>)
+                ((OAuth2Authentication) principal).getUserAuthentication().getDetails();
+        String userName = (String) details.get("name");
+        return "hi " + userName + " Welcome Shop Mobile !! ";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
